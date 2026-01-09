a3c8cd182000577da90d243924d3e15de4eaa1f7
Oswaldo Alonso <jesusalonso_l@hotmail.com>
2026-01-08 20:38:51 -0600

    actualizacion js modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -9,9 +9,19 @@ actions.forEach((a) =>
     switch (targetAttribute) {
       case "open":
         modal.toggleAttribute("open");
+        modal.classList.add("show");
+        setTimeout(() => {
+            modal.classList.add("active")
+            modal.classList.remove("show");
+        }, 250);
         break;
       case "close":
-        modal.removeAttribute("open");
+        modal.classList.add("hide");
+        setTimeout(() => {
+          modal.classList.remove("hide");
+          modal.removeAttribute("open");
+          modal.classList.remove("active")
+        }, 250);
         break;
     }
   })
